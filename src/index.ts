32873abe03ff43aefc03aeda941a7fbf5296626a
Labeeb Ahmad <labeebahmad201@gmail.com>
2021-10-05 14:34:15 +0500

    removed unexpected console.log

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
-console.log('Hello world!');
+import express, { Application } from 'express';
+import dotenv, { DotenvConfigOutput } from 'dotenv';
 
-import express, {Application} from 'express'; 
-import dotenv, {DotenvConfigOutput} from 'dotenv';
+const app: Application = express();
 
-const app:Application = express();
-
-const config: DotenvConfigOutput = dotenv.config({ path : __dirname + '/../.env' });
+const config: DotenvConfigOutput = dotenv.config({
+  path: __dirname + '/../.env',
+});
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, ()=>{
-    console.log(`Server is listening at port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening at port ${PORT}`);
 });
-
-
-
-
-
-
