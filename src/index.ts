359efa7334ffe20843f0ae68ea7be1b850a23ed7
Labeeb Ahmad <labeebahmad201@gmail.com>
2021-10-05 14:32:49 +0500

    added express added .env & .env.example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
 console.log('Hello world!');
+
+import express, {Application} from 'express'; 
+import dotenv, {DotenvConfigOutput} from 'dotenv';
+
+const app:Application = express();
+
+const config: DotenvConfigOutput = dotenv.config({ path : __dirname + '/../.env' });
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening at port ${PORT}`);
+});
+
+
+
+
+
+
